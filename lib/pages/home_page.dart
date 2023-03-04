9263ec6773c8c37fdfb4214d74d447237f08ed3a
knyongki <yyongkisetiawan@gmail.com>
2023-03-04 23:10:27 +0700

    memisahkan floatinwidget dan homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,6 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:optima/shared/theme.dart';
+import '../widgets/floating_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -141,102 +140,6 @@ class _HomePageState extends State<HomePage> {
           child: const Icon(Icons.add),
         ),
       ),
-    );
-  }
-}
-
-class FloatingWidget extends StatefulWidget {
-  final VoidCallback hideCallback;
-  const FloatingWidget({Key? key, required this.hideCallback})
-      : super(key: key);
-
-  @override
-  State<FloatingWidget> createState() => _FloatingWidgetState();
-}
-
-class _FloatingWidgetState extends State<FloatingWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-          child: Container(
-            color: Colors.black.withOpacity(0.2),
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-          ),
-        ),
-        Positioned(
-          child: Container(
-            padding: const EdgeInsets.all(10.0),
-            margin: EdgeInsets.only(
-              left: defaultMargin,
-              right: defaultMargin,
-              top: 30,
-              bottom: 100,
-            ),
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            color: Colors.white,
-            child: Column(
-              children: [
-                const Text(
-                  'Kelas Baru',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-                // const SizedBox(height: 20),
-                Expanded(child: Container()),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Expanded(
-                      child: ElevatedButton(
-                        onPressed: () {
-                          widget.hideCallback();
-                        },
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: kWhiteColor,
-                            side: BorderSide(
-                              color: kPrimaryColor,
-                              width: 1,
-                            )),
-                        child: Text(
-                          'batal',
-                          style: textStyle.copyWith(
-                            color: kPrimaryColor,
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    Expanded(
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: kPrimaryColor,
-                        ),
-                        onPressed: () {},
-                        child: Text(
-                          'simpan',
-                          style: textStyle.copyWith(
-                            color: kWhiteColor,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
