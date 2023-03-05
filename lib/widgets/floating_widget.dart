90deac5228298da6391483dfb37585fe4627c711
knyongki <yyongkisetiawan@gmail.com>
2023-03-05 21:12:47 +0700

    menambahakan custom_text_form_file dan modifikasi floating widget

--- lib/widgets/floating_widget.dart
+++ lib/widgets/floating_widget.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:optima/shared/theme.dart';
+import 'package:optima/widgets/text_form_file.dart';
 
 class FloatingWidget extends StatefulWidget {
   final VoidCallback hideCallback;
@@ -34,21 +35,86 @@ class _FloatingWidgetState extends State<FloatingWidget> {
               top: 30,
               bottom: 100,
             ),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(defaultRadius)),
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height,
-            color: Colors.white,
             child: Column(
               children: [
-                const Text(
-                  'Kelas Baru',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
+                const Center(
+                  child: Text(
+                    'Kelas Baru',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
                   ),
                 ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          // contentPadding: const EdgeInsets.symmetric(horizontal: 10),
+                          hintText: 'class name',
+                          hintStyle: textStyle.copyWith(
+                            color: kGreyTextColor,
+                            fontSize: 14,
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: kPrimaryColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Silabus',
+                              style: textStyle.copyWith(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                // aksi saat tombol tambah ditekan
+                              },
+                              icon: Icon(Icons.add, color: kGreyTextColor),
+                              splashRadius: 24,
+                              padding: EdgeInsets.zero,
+                              constraints: BoxConstraints(),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const CustomTextFormFile(),
+                      const CustomTextFormFile(),
+                      const CustomTextFormFile(),
+                      const CustomTextFormFile(),
+                      const CustomTextFormFile(),
+                      const CustomTextFormFile(),
+                      const CustomTextFormFile(),
+                      const CustomTextFormFile(),
+                      const CustomTextFormFile(),
+                      const CustomTextFormFile(),
+                    ],
+                  ),
+                ),
+
                 // const SizedBox(height: 20),
-                Expanded(child: Container()),
+                // const Expanded(
+                //   child: SizedBox(),
+                // ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
